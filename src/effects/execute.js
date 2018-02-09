4606c2bd1032d5aa64f2a22c78e7bbb2dac3f7b6
Mike Davis <mike@michaeljacobdavis.com>
2018-02-08 20:46:00 -0800

    Remove blah

--- src/effects/execute.js
+++ src/effects/execute.js
@@ -3,7 +3,6 @@ const check = require("./check");
 const write = require("./write");
 
 module.exports = function*(url) {
-  console.log("blah");
   const response = yield call(check, url);
   return yield call(write, response);
 };
